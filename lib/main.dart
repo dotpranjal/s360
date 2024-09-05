b8dab9fe7dbecc65fddeec3e09464fd18c355fd2
dotpranjal <chotachattri69@gmail.com>
2024-09-05 18:39:11 +0530

    changes to homepage

--- lib/main.dart
+++ lib/main.dart
@@ -81,6 +81,22 @@ class SOSPage extends StatelessWidget{
   }
 }
 
+class NewsPage extends StatelessWidget{
+  const NewsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News'),
+      ),
+      // body: SingleChildScrollView(
+      //   child: Column(),
+      // )
+    );
+  }
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -92,14 +108,36 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search))
+          IconButton(onPressed: (){Navigator.pushNamed(context, '/settings');}, icon: const Icon(Icons.settings,size: 30.0,))
         ],
       
       ),
-      body: const Center(
-        child: Text(
-          'Home Page',
-          style: TextStyle(fontSize: 24, fontFamily: 'Roboto',fontStyle: FontStyle.italic),
+      body: SingleChildScrollView(
+        child:Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container( 
+                  margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  decoration: BoxDecoration(color: Colors.grey.shade900, borderRadius: BorderRadius.circular(10.0)),
+                  width: MediaQuery.of(context).size.width/2.2,
+                  height:170),
+
+                Container( 
+                  margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  decoration: BoxDecoration(color: Colors.grey.shade900, borderRadius: BorderRadius.circular(10.0)),
+                  width: MediaQuery.of(context).size.width/2.2,
+                  height:170),
+              ],
+            ),
+            Container(decoration: BoxDecoration(color: Colors.grey.shade900, borderRadius: BorderRadius.circular(10.0)),
+                      height: MediaQuery.of(context).size.height-500,
+                      width: MediaQuery.of(context).size.width-40,
+                      margin: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+                      ),
+            const Text('Made with love.',)
+          ],
         ),
       ),
       bottomNavigationBar: Container(
@@ -112,7 +150,7 @@ class MyHomePage extends StatelessWidget {
             IconButton(onPressed: () {Navigator.pushNamed(context, '/home');}, icon: const Icon(Icons.home, size: 30.0)),
             IconButton(onPressed: () {Navigator.pushNamed(context, '/sos');}, icon: const Icon(Icons.phone, size: 30.0)),
             IconButton(onPressed: () {Navigator.pushNamed(context, '/location');}, icon: const Icon(Icons.location_on, size: 30.0)),
-            IconButton(onPressed: () {Navigator.pushNamed(context, '/settings');}, icon: const Icon(Icons.settings, size: 30.0))
+            IconButton(onPressed: () {Navigator.pushNamed(context, '/news');}, icon: const Icon(Icons.feed, size: 30.0))
           ],
         ),
       ),
@@ -131,7 +169,8 @@ class MyApp extends StatelessWidget {
         '/': (context)=> const MyHomePage(title: 'S360'),
         '/sos': (context)=> const SOSPage(),
         '/location': (context)=> const LocationPage(),
-        '/settings': (context)=> const SettingsPage()
+        '/settings': (context)=> const SettingsPage(),
+        '/news': (context)=> const NewsPage()
 
       },
       title: 'Flutter Demo',
